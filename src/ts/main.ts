ee33a4f380af8123013708c4e8c56f4c4c2b9a01
Melvin Kempe <k3mp314@gmail.com>
2023-01-30 14:06:56 +0100

    All tests done

---
+++ src/ts/main.ts
@@ -0,0 +1,85 @@
+import { addTodo, changeTodo, removeAllTodos } from "./functions";
+import { Todo } from "./models/Todo";
+
+let todos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+console.log(todos);
+
+document.getElementById("clearTodos")?.addEventListener("click", () => {
+  clearTodos(todos);
+});
+
+(document.getElementById("newTodoForm") as HTMLFormElement)?.addEventListener(
+  "submit",
+  (e: SubmitEvent) => {
+    e.preventDefault();
+
+    let todoText: string = (
+      document.getElementById("newTodoText") as HTMLInputElement
+    ).value;
+    console.log("Todos when creating", todos);
+
+    createNewTodo(todoText, todos);
+  }
+);
+
+export function createNewTodo(todoText: string, todos: Todo[]) { //Done
+  let result = addTodo(todoText, todos);
+
+  if (result.success) {
+    exports.createHtml(todos);
+  } else {
+    exports.displayError(result.error, true);
+  }
+}
+
+export function createHtml(todos: Todo[]) {
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  let todosContainer: HTMLUListElement = document.getElementById(
+    "todos"
+  ) as HTMLUListElement;
+
+  todosContainer.innerHTML = "";
+
+  for (let i = 0; i < todos.length; i++) {
+    let li: HTMLLIElement = document.createElement("li");
+
+    if (todos[i].done) {
+      li.classList.add("todo__text--done");
+    }
+
+    li.classList.add("todo__text");
+    li.innerHTML = todos[i].text;
+    li.addEventListener("click", () => {
+      toggleTodo(todos[i]);
+    });
+
+    todosContainer.appendChild(li);
+  }
+}
+
+export function toggleTodo(todo: Todo) {
+  changeTodo(todo);
+  exports.createHtml(todos);
+}
+
+export function displayError(error: string, show: boolean) { //Done
+  let errorContainer: HTMLDivElement = document.getElementById(
+    "error"
+  ) as HTMLDivElement;
+
+  errorContainer.innerHTML = error;
+
+  if (show) {
+    errorContainer.classList.add("show");
+  } else {
+    errorContainer.classList.remove("show");
+  }
+}
+
+export function clearTodos(todos: Todo[]) { //Done
+  removeAllTodos(todos);
+  exports.createHtml(todos);
+}
+
+//createHtml(todos);
